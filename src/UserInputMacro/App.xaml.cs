50dd6e1d37132652a26ad98194be63ac0e35d3f4
hukatama024e <physaliaphysalisdev@gmail.com>
2017-03-06 02:50:41 +0900

    Add support command line option to execute script with no executing main window

--- src/UserInputMacro/App.xaml.cs
+++ src/UserInputMacro/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
 
 namespace UserInputMacro
 {
@@ -7,5 +10,47 @@ namespace UserInputMacro
 	/// </summary>
 	public partial class App : Application
 	{
+		[DllImport( "Kernel32.dll" )]
+		private	 static extern bool AttachConsole( int processId );
+
+		const int ATTACH_PARENT_PROCESS = -1;
+
+		private async void Application_StartupAsync( object sender, StartupEventArgs e )
+		{
+			try {
+				// when no arguments are specified, execute main window 
+				if( e.Args.Length == 0 ) {
+					var window = new MainWindow();
+					window.Show();
+					return;
+				}
+				else if( e.Args.Length >= 2 ) {
+					Usage();
+				}
+
+				Regex scriptArgPattern = new Regex( "-script=(?<scriptPath>.+)" );
+				Match argsChacker = scriptArgPattern.Match( e.Args[ 0 ] );
+
+				if( argsChacker.Success ) {
+					await ScriptExecuter.ExecuteAsync( argsChacker.Groups[ "scriptPath" ].Value );
+				}
+				else {
+					Usage();
+				}
+			}
+			catch( Exception ex ) {
+				Console.WriteLine( ex );
+			}
+
+			Current.Shutdown();
+		}
+
+		private void Usage()
+		{
+			AttachConsole( ATTACH_PARENT_PROCESS );
+			Console.WriteLine( "Usage: UserInputMacro <option>" );
+			Console.WriteLine( "[option list]" );
+			Console.WriteLine( "script=<scirpt path>: Command line mode and only execute script" );
+		}
 	}
 }
